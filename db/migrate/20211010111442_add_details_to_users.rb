e66e663689a0dfbf4121b70957597283f3d97221
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-10-10 11:18:13 +0000

    gem導入

---
+++ db/migrate/20211010111442_add_details_to_users.rb
@@ -0,0 +1,7 @@
+class AddDetailsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :is_deleted, :boolean, default: false, null: false
+    add_column :users, :introduction, :text
+    add_column :users, :image_id, :string
+  end
+end
