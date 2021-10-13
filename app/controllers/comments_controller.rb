5b3645fe3a2d8c01f99ba749f088b8b588fd36e8
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-10-13 10:36:30 +0000

    コメント周辺実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,29 @@
 class CommentsController < ApplicationController
   def create
+    @knowledge = Knowledge.find(params[:knowledge_id])
+    @comment = Comment.new
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.knowledge_id = @knowledge.id
+    if @comment.save
+      flash[:notice] = "You have created comment successfully."
+      redirect_to request.referer
+    else
+      @knowledge = Knowledge.find(params[:id])
+      @phrase = Phrase.find_by(id: @knowledge.phrase_id)
+      @comments = Comment.where(knowledge_id: @knowledge.id)
+      render 'knowledges/show'
+    end
   end
   
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to request.referer
+  end
+  
+  private
+  def comment_params
+    params.permit(:content)    
   end
 end
--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -35,6 +35,7 @@ class KnowledgesController < ApplicationController
   def show
     @knowledge = Knowledge.find(params[:id])
     @phrase = Phrase.find_by(id: @knowledge.phrase_id)
+    @comments = Comment.where(knowledge_id: @knowledge.id)
   end
 
   def destroy
