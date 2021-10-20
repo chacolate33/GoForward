b66ff1a16775bcf82396bea4cad71512203a32f7
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-10-20 00:49:22 +0000

    管理者側ユーザー詳細画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,14 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    # 以下所属グループ一覧表示の記述
+    @group_users = GroupUser.where(user_id: @user.id)
+    @groups = []
+    @group_users.each do |group_user|
+      group = Group.find_by(id: group_user.group_id)
+      @groups.push(group)
+    end
   end
 
   def update
