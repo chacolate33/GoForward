feea6203ee3236157cb3fd3e69813790e60b5313
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-10-25 04:24:27 +0000

    add sort in phrases

--- app/controllers/admin/phrases_controller.rb
+++ app/controllers/admin/phrases_controller.rb
@@ -1,7 +1,20 @@
 class Admin::PhrasesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    @phrases = Phrase.where(group_id: @group.id)
+    if params[:sort_abc]
+      @phrases = Phrase.where(group_id: @group.id).order(content: "ASC")
+    elsif params[:sort_aiu]
+      @phrases = Phrase.where(group_id: @group.id).order(japanese: "ASC")
+    elsif params[:sort_knowledge]
+      @phrases = Phrase.where(group_id: @group.id).includes(:posted_phrases).sort {|a, b|
+          b.posted_phrases.includes(:knowledges).size <=>
+          a.posted_phrases.includes(:knowledges).size
+        }
+    elsif params[:sort_new]
+      @phrases = Phrase.where(group_id: @group.id).order(created_at: "DESC")
+    else
+      @phrases = Phrase.where(group_id: @group.id)
+    end
   end
 
   def show
