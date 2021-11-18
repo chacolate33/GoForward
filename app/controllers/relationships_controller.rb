32a4485d6a3f35fa69189c0c7e56158eb97eb08b
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-11-18 13:29:09 +0000

    fix follow_index

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,12 +13,12 @@ class RelationshipsController < ApplicationController
 
   # フォロー・フォロワー一覧表示
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings.page(params[:page]).per(20)
+    @user = User.find(params[:user_id])
+    @users = @user.followings.page(params[:page]).per(20)
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers.page(params[:page]).per(20)
+    @user = User.find(params[:user_id])
+    @users = @user.followers.page(params[:page]).per(20)
   end
 end
